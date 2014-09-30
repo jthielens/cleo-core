be0fad7e93a4da9efc0037550d55194b9e3049ef
John Thielens <jthielens@cleo.com>
2014-09-30 15:56:39 -0400

    updated service name

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 unset DISPLAY
-harmony=`sed -n '/^env /s/.*=\s*//p' /etc/init/harmony.conf`
+harmony=`sed -n '/^env\s*CLEOHOME=/s/.*=\s*//p' /etc/init/cleo-harmony.conf`
 here=`dirname $0`
 $harmony/jre/bin/java -cp $here/cc-shell.jar:$harmony/lib/\*:$harmony/lib/help/\*:$harmony/webserver/AjaxSwing/lib/ajaxswing.jar:$harmony/lib/hibernate/\*:$harmony/lib/secureshare/\*:$harmony/lib/ext/\* com.sodiumcow.cc.shell.Shell -h $harmony -p h -m client "$@"
