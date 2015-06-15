e3ff57e8740ec8f49ae115b810f03d4dca823de8
John Thielens <jthielens@cleo.com>
2015-06-15 17:10:46 -0500

    updated shell.sh to com.cleo.labs and Maven

--- shell.sh
+++ shell.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-unset DISPLAY
-harmony=`sed -n '/^env\s*CLEOHOME=/s/.*=\s*//p' /etc/init/cleo-harmony.conf`
 here=`dirname $0`
-$harmony/jre/bin/java -cp $here/cc-shell.jar:$harmony/lib/\*:$harmony/lib/help/\*:$harmony/webserver/AjaxSwing/lib/ajaxswing.jar:$harmony/lib/hibernate/\*:$harmony/lib/secureshare/\*:$harmony/lib/json/\*:$harmony/lib/ext/\*:$harmony/lib/uri/\* com.cleo.labs.api.shell.Shell -h $harmony -p h -m client "$@"
+if [ "$1" = "update" ]; then
+    snapshot=`wget -nv -q -O- http://contd.cleo.com/nexus/content/repositories/snapshots/com/cleo/labs/cleo-labs-util/0.0.1-SNAPSHOT/maven-metadata.xml | sed -n '0,/<value>/s/.*<value>\(.*\)<\/value>/\1/p'`
+    wget -nv -q -O $here/cleo-labs-util-0.0.1-SNAPSHOT.jar http://contd.cleo.com/nexus/content/repositories/snapshots/com/cleo/labs/cleo-labs-util/0.0.1-SNAPSHOT/cleo-labs-util-$snapshot.jar
+    snapshot=`wget -nv -q -O- http://contd.cleo.com/nexus/content/repositories/snapshots/com/cleo/labs/cleo-labs-api-shell/0.0.1-SNAPSHOT/maven-metadata.xml | sed -n '0,/<value>/s/.*<value>\(.*\)<\/value>/\1/p'`
+    wget -nv -q -O $here/cleo-labs-api-shell-0.0.1-SNAPSHOT.jar http://contd.cleo.com/nexus/content/repositories/snapshots/com/cleo/labs/cleo-labs-api-shell/0.0.1-SNAPSHOT/cleo-labs-api-shell-$snapshot.jar
+else
+    service='cleo-harmony'
+    if [ -e "/etc/init/$1.conf" ]; then
+        service=$1
+        shift
+    fi
+    harmony=`sed -n '/^env\s*CLEOHOME=/s/.*=\s*//p' /etc/init/$service.conf`
+    unset DISPLAY
+    classpath=$here/cleo-labs-api-shell-0.0.1-SNAPSHOT.jar:$here/cleo-labs-util-0.0.1-SNAPSHOT.jar:$harmony/lib/\*:$harmony/lib/help/\*:$harmony/webserver/AjaxSwing/lib/ajaxswing.jar:$harmony/lib/hibernate/\*:$harmony/lib/secureshare/\*:$harmony/lib/json/\*:$harmony/lib/ext/\*:$harmony/lib/uri/\*
+    (cd $harmony; ./jre/bin/java -cp $classpath com.cleo.labs.api.shell.Shell -h . -p h -m client "$@")
+fi
