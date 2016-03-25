52d81de3d26ef7de3609d90df998e8d7e075ffc1
John Thielens <jthielens@jthielens1.lan.cleo.com>
2016-03-25 12:13:42 -0400

    add OmniHost as HostType.LOCAL_USER

--- src/main/java/com/cleo/labs/api/constant/HostType.java
+++ src/main/java/com/cleo/labs/api/constant/HostType.java
@@ -22,9 +22,10 @@ public enum HostType {
     EBICS      (Protocol.HTTP_CLIENT,   Packaging.EBICS, "..G", "EBICS"),
     FASP       (Protocol.FASP_CLIENT,   Packaging.NONE,  ".DG", "fasp",    "Generic fasp"),
     HSP        (Protocol.HSP,           Packaging.NONE,  "...", "HSP"),
-    LOCAL_FTP  (Protocol.FTP_SERVER,    Packaging.NONE,  "L..", "FTP listener",     "Local FTP Users"),
-    LOCAL_HTTP (Protocol.HTTP_SERVER,   Packaging.NONE,  "L..", "HTTP listener",    "Local HTTP Users"),
-    LOCAL_SFTP (Protocol.SSHFTP_SERVER, Packaging.NONE,  "L..", "SSH FTP listener", "Local SSH FTP Users");
+    LOCAL_USER (Protocol.HTTP_SERVER,   Packaging.NONE,  "L..", "FTP, SSH FTP, HTTP listener", "Users"),
+    LOCAL_FTP  (Protocol.FTP_SERVER,    Packaging.NONE,  "L..", "FTP listener",                "Local FTP Users"),
+    LOCAL_HTTP (Protocol.HTTP_SERVER,   Packaging.NONE,  "L..", "HTTP listener",               "Local HTTP Users"),
+    LOCAL_SFTP (Protocol.SSHFTP_SERVER, Packaging.NONE,  "L..", "SSH FTP listener",            "Local SSH FTP Users");
 
     public final Protocol  protocol;
     public final Packaging packaging;
